3b39a2b3f0a2715dd704d6bdcc5d5225dbcfc029
Souverain73 <Souverain73@gmail.com>
2018-12-24 01:14:18 +0300

    Add default and required parameters.

--- src/main/java/com/souverain73/ImageConverter/ArgumentTypes.java
+++ src/main/java/com/souverain73/ImageConverter/ArgumentTypes.java
@@ -5,4 +5,5 @@ public class ArgumentTypes {
     public static ArgumentParser longVal = Long::valueOf;
     public static ArgumentParser floatVal = Double::valueOf;
     public static ArgumentParser stringVal = String::valueOf;
+    public static ArgumentParser booleanValue = Boolean::valueOf;
 }
--- src/main/java/com/souverain73/ImageConverter/Main.java
+++ src/main/java/com/souverain73/ImageConverter/Main.java
@@ -1,36 +1,55 @@
 package com.souverain73.ImageConverter;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 
-import static com.souverain73.ImageConverter.ArgumentTypes.intVal;
+import static com.souverain73.ImageConverter.ArgumentTypes.*;
 import static com.souverain73.ImageConverter.ArgumentsConfigItem.item;
 import static java.util.List.of;
 
 public class Main {
+    public static class Params {
+        public int width;
+        public int height;
+        public int density;
+        public String fileName;
+        public boolean useFormattedOutput;
+    }
+
+    public static Params params = new Params();
+
     public static void main(String[] args) {
         ArgumentsConfig programArgumentsConfig = new ArgumentsConfig(
                 item("width", of("-w", "--width"), intVal, "Width of printed image (mm)"),
                 item("height", of("-h", "--height"), intVal, "Height of printed image (mm)"),
                 item("density", of("-d", "--density"), intVal, "Density (dots per mm)"),
-                item("fileName", of("-f", "--file"), intVal, "Name of input file (default source.png)")
+                item("fileName", of("-f", "--file"), stringVal, "Name of input file (default source.png)"),
+                item("formattedOutput", of("-F", "--formatOutput"), booleanValue, "Format output (end of lines, etc.)")
         );
-        Map<String, Object> params = handleParams(programArgumentsConfig, args);
+        Map<String, Object> parsedParams = handleParams(programArgumentsConfig, args);
 
-        System.out.println("params = " + params.toString());
+        params.width = (int) getOrThrow(parsedParams, "width");
+        params.height = (int) getOrThrow(parsedParams, "height");
+        params.density = (int) parsedParams.getOrDefault("density", 8);
+        params.fileName = (String) parsedParams.getOrDefault("fileName", "source.png");
+        params.useFormattedOutput = (boolean) parsedParams.getOrDefault("formattedOutput", false);
     }
 
     private static Map<String, Object> handleParams(ArgumentsConfig programArgumentsConfig, String[] args) {
-        if (args.length == 0){
+        if (args.length == 0) {
             return Collections.emptyMap();
         }
 
-        if ("/?".equals(args[0]) || "--help".equals(args[0])){
+        if ("/?".equals(args[0]) || "--help".equals(args[0])) {
             System.out.println(programArgumentsConfig.toString());
             System.exit(0);
         }
 
         return new ProgramArgumentsParser(programArgumentsConfig).parseParams(args);
+    }
+
+    private static Object getOrThrow(Map<String, Object> map, String key) {
+        return Objects.requireNonNull(map.get(key));
     }
 }
